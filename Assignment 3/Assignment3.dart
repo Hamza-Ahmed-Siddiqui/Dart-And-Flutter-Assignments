e8d0eeecd34121f3da551f3e229e0cc04471a729
Hamza-Ahmed-Siddiqui <princehamzaahmed9@gmail.com>
2021-09-25 23:29:30 +0500

    Assignment 3 Completedgit status!

---
+++ Assignment 3/Assignment3.dart
@@ -0,0 +1,158 @@
+void main(){
+
+// ==================== Practice ==================
+//   var primeNumber = 3;
+//   if(primeNumber%primeNumber){
+//     print("The Number is Prime ");
+//   }else{
+//     print("The Number is Composite ");
+//   }
+  
+//   var n = 4; 
+//   var i = 2;
+//   if (n ==1){
+//     print("The ");
+//   }
+//   if(n == 2) {
+//     print("The Number is 2");
+//   }
+//    for(var i = 2; i < n; i++) {
+//       if(n % i == 0) {
+//         print("The number is Prime");
+//       }
+//     else {
+//     print("The Number is not prime");
+//    }
+//    }
+  
+  
+  
+  
+//   print("================ Answer#2 ==============");
+// print("=============== Print To Difference Two List ==============");
+  
+//   var arr1 = [1, 2, 3, 4, 5, 6, 7];
+//   var arr2 = [3, 5, 6, 7, 9, 10];
+//   var diff = [];
+
+// for (var i = 0; i < arr1.length; i++) {
+//   if (arr2.indexOf(arr1[i]) == -1 && diff.indexOf(arr1[i]) == -1) {
+//     diff.add(arr1[i]);
+//     print(diff);
+//   }
+// }
+  
+  
+  
+  
+// print("================ Answer#3 ==============");
+// print("=============== Print To Only Even Elements In List ==============");
+  
+//   var arr = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+//   var evenArr = [];
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     evenArr.add(arr[i]);
+//     print(evenArr);
+//   }
+// }
+  
+  
+
+  
+//   print("================ Answer#5 ==============");
+// print("=============== Print Table ==============");
+//   var numberr = 7;
+  
+//   for(var i = 1; i<=15 ; i++){
+//     print( " ${numberr} * ${i} = ${numberr*i}");
+//   }
+  
+  
+  
+//     print("================ Answer#6 ==============");
+// print("=============== Print Items Using For Loop ==============");
+  
+//   var fruits = ["Apple","Mango","Orange","Strawbery","Banana","Grapes","Pine Apple"];
+//   for(var i = 0; i<fruits.length;i++){
+//     print(fruits[i]);
+//   }
+  
+  
+  
+  
+  
+//   print("================ Answer#7 ==============");
+// print("=============== Print Multiple Of 5 ==============");
+//   var i;
+//   for(i = 5 ; i <=100; i=i+5){
+//     print(i);
+//   }
+  
+  
+//   print("================ Answer#8 ==============");
+// print("======== Print Conerter Farenhite into Celsius And Celsius Into Farenhite ==============");
+  
+//   var cles = 25;
+//   var faren = cles * 9 / 5 + 32;
+//   var convertToCelsius = (faren-32)*5/9;
+//   print("The Farenhite Temperature is ${faren}");
+//   print("The Celsius Temperature is ${convertToCelsius}");
+  
+  
+  
+  
+//   print("================ Answer#9 ==============");
+// print("=============== Print To Calculator ==============");
+  
+//   int firstNumber = 5;
+//   int secondNumber = 5;
+//   var operator = "+";
+//   if(operator == "+"){
+//     print(firstNumber+secondNumber);
+//   }else if(operator == "-"){
+//     print(firstNumber-secondNumber);
+//     }else if(operator == "*"){
+//     print(firstNumber*secondNumber); 
+//   }else if(operator == "/"){
+//     print(firstNumber/secondNumber);
+//   } else {
+//     print("False");
+//   }
+    
+  
+  
+//   print("================ Answer#10 ==============");
+//   print("=========== Print To Find Out The Vowels Or Not Vowels !!! =========");
+  
+//     String check = "a";
+//   if( check=="a" || check=="e"|| check=="i" || check=="o" || check=="u" ){
+//     print(" The Character you entered is Vowel.");
+//   } else {
+//     print("The Character you entered is Not Vowel.");
+//   }
+  
+
+  
+  
+//   print("=============== Alhamdullilah Assignment 3 Completed" ===========);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  }
